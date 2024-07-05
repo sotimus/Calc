f0cf194bc70630144213bd20bb75b3b69593516a
Sumitosh <sumitosh.pal@gmail.com>
2024-07-05 10:57:37 +0530

    Created add and sub methods in Calculator

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator{
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int sub(int a, int b){
+        return a - b;
+    }
+
+    public static void main(String[] args){
+        Calculator c = new Calculator();
+        int a = 15, b = 10;
+        System.out.println("For input a: " + a + " and b: " + b);
+        System.out.println("Sum -> " + c.add(a,b));
+        System.out.println("Subtraction -> " + c.sub(a,b));
+
+    }
+}
