b8a615446d5366f864d27e141ec25b9361f15922
Sumitosh <sumitosh.pal@gmail.com>
2024-07-05 11:21:35 +0530

    mul method added

--- Calculator.java
+++ Calculator.java
@@ -8,12 +8,17 @@ public class Calculator{
         return a - b;
     }
 
+    public int mul(int a, int b){
+        return a * b;
+    }
+
     public static void main(String[] args){
         Calculator c = new Calculator();
         int a = 15, b = 10;
         System.out.println("For input a: " + a + " and b: " + b);
         System.out.println("Sum -> " + c.add(a,b));
         System.out.println("Subtraction -> " + c.sub(a,b));
+        System.out.println("Multiplication -> " + c.mul(a,b));
 
     }
 }
