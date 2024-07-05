06dfe2ea1a151d853a217d1e4ac66cb5643ec998
Sumitosh <sumitosh.pal@gmail.com>
2024-07-05 16:21:49 +0530

    Div method removed

--- Calculator.java
+++ Calculator.java
@@ -8,13 +8,6 @@ public class Calculator{
         return a - b;
     }
 
-    public int mul(int a, int b){
-        return a * b;
-    }
-
-    public int div(int a, int b){
-        return a / b;
-    }
 
     public static void main(String[] args){
         Calculator c = new Calculator();
@@ -22,8 +15,7 @@ public class Calculator{
         System.out.println("For input a: " + a + " and b: " + b);
         System.out.println("Sum -> " + c.add(a,b));
         System.out.println("Subtraction -> " + c.sub(a,b));
-        System.out.println("Multiplication -> " + c.mul(a,b));
-        System.out.println("Division -> " + c.div(a,b));
+
 
     }
 }
