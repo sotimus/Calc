8f5dcff66162d23657d4a453bae5dd4492097cc8
Sumitosh <sumitosh.pal@gmail.com>
2024-07-05 16:13:31 +0530

    Div method added

--- Calculator.java
+++ Calculator.java
@@ -12,6 +12,10 @@ public class Calculator{
         return a * b;
     }
 
+    public int div(int a, int b){
+        return a / b;
+    }
+
     public static void main(String[] args){
         Calculator c = new Calculator();
         int a = 15, b = 10;
@@ -19,6 +23,7 @@ public class Calculator{
         System.out.println("Sum -> " + c.add(a,b));
         System.out.println("Subtraction -> " + c.sub(a,b));
         System.out.println("Multiplication -> " + c.mul(a,b));
+        System.out.println("Division -> " + c.div(a,b));
 
     }
 }
